bcd3fb045f4a0fc928e976cd4f0c9cb8bd7ce6f0
michaelEmeka <mikelonu15@gmail.com>
2024-10-17 05:10:13 +0100

    Secure credentials

--- backend/greencart/settings.py
+++ backend/greencart/settings.py
@@ -24,13 +24,13 @@ ALLOWED_HOSTS = []
 
 # Email
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = "greencart.ecotrybe@gmail.com"
-EMAIL_HOST_PASSWORD = "mvpgpxbikxmazshl"
-DEFAULT_FROM_EMAIL = "GreenCart <greencart.ecotrybe@gmail.com>"
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 # Application definition
 
